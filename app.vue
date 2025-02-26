1bd840d732d4617040e935b983de323f05a98246
Vir Kothari <73329256+Vir-8@users.noreply.github.com>
2025-02-27 03:28:54 +0530

    Update text (#19)

--- app.vue
+++ app.vue
@@ -15,18 +15,18 @@
     title: 'Deep Research (powered by mem0)',
     meta: [
       // Primary Meta Tags
-      { name: 'title', content: 'Deep Research (powered by mem0)' },
+      { name: 'title', content: 'Personalized Deep Research (powered by mem0)' },
       { name: 'description', content: 'Advanced deep research leveraging mem0 to create highly tailored reports designed just for you. Get in-depth deep research reports optimized for your needs.' },
 
       // Open Graph / Facebook
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: 'https://deep-research.mem0.ai' },
-      { property: 'og:title', content: 'Deep Research (powered by mem0)' },
+      { property: 'og:title', content: 'Personalized Deep Research (powered by mem0)' },
       { property: 'og:description', content: 'Advanced deep research leveraging mem0 to create highly tailored reports designed just for you. Get in-depth deep research reports optimized for your needs.' },
 
       // Twitter
       { name: 'twitter:url', content: 'https://deep-research.mem0.ai' },
-      { name: 'twitter:title', content: 'Deep Research (powered by mem0)' },
+      { name: 'twitter:title', content: 'Personalized Deep Research (powered by mem0)' },
       { name: 'twitter:description', content: 'Advanced deep research leveraging mem0 to create highly tailored reports designed just for you. Get in-depth deep research reports optimized for your needs.' },
     ]
   });
